365e7b667a167e1d0c4530562a765e4848a7ff6a
roosterMAP <lead.acrysoft@gmail.com>
2025-05-06 12:05:56 -0700

    expose functions and classes
    expose functions and classes

--- __init__.py
+++ __init__.py
@@ -14,3 +14,7 @@ bl_info = {
 	"tracker_url": "https://github.com/yourusername/rmlib/issues",
 	"category": "Library",
 }
+
+from .item import rmMesh, iter_edit_meshes, iter_selected_meshes
+from .elem_set import rmVertexSet, rmEdgeSet, rmPolygonSet, rmUVLoopSet, clear_tags
+from .util import rmViewport, rmCustomOrientation, line2_dist, PlaneDistance, ReflectionMatrix, Angle2, AlmostEqual, AlmostEqual_v2, ProjectVector, CCW_Angle2D, HSV_to_RGB, EaseOutCircular, EaseInCircular, TriangleArea
