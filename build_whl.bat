67cc6929f0f6707ffd1532a71f658eeb9c172886
roosterMAP <lead.acrysoft@gmail.com>
2025-05-30 10:34:45 -0700

    version update
    - updated build_whl.bat to use blenders provided python
    - incremented toml

--- build_whl.bat
+++ build_whl.bat
@@ -1,1 +1,28 @@
-python -m build
+@echo off
+setlocal
+
+:: Correctly assign path
+set "BLENDER_DIR=C:\Program Files\Blender Foundation\Blender 4.4"
+set "BLENDER_PY=%BLENDER_DIR%\4.4\python\bin\python.exe"
+
+:: Check if Blender Python exists
+if exist "%BLENDER_PY%" (
+    echo Found Blender Python at %BLENDER_PY%
+) else (
+    echo Blender Python not found at %BLENDER_PY%
+    echo Please update BLENDER_DIR in the script.
+    endlocal
+    echo Press any key to exit . . .
+    pause >nul
+    exit /b 1
+)
+
+:: Proceed with build or other commands
+"%BLENDER_PY%" -m ensurepip
+"%BLENDER_PY%" -m pip install --upgrade pip build
+"%BLENDER_PY%" -m build %*
+
+endlocal
+echo Press any key to exit . . .
+pause >nul
+exit /b 1
